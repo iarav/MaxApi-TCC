42f8b478c8c040cb027c837feb3de79c4176347a
Iara <iaravaneti9@gmail.com>
2024-10-02 14:34:16 -0300

    correcting CORS

--- api/main.py
+++ api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from .db import Engine, Base
 from .endpoints import FocalQuestions, Chat
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=Engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(FocalQuestions.router, prefix="/api/focalQuestion", tags=["focalQuestion"])
 app.include_router(Chat.router, prefix="/api/chat", tags=["chat"])
